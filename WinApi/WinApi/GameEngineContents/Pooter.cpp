67c195829d444ec2eadf4d06280460e9594555b9
chcjsdud 조해근 <chcjsdud@naver.com>
2023-03-02 17:31:36 +0900

    총알 방향 업데이트

--- WinApi/WinApi/GameEngineContents/Pooter.cpp
+++ WinApi/WinApi/GameEngineContents/Pooter.cpp
@@ -114,7 +114,7 @@ void Pooter::Update(float _DeltaTime)
 				M_Pooter->ChangeAnimation("M_Pooter_Shoot");
 				float4 PlayerPos_P = Isaac::MainPlayer->GetPos();
 				BloodTear* PooterAtt = GetLevel()->CreateActor<BloodTear>();
-				PooterAtt->SetBloodMoveDir(PlayerPos_P);
+				PooterAtt->SetBloodMoveDir(PlayerPos_P - GetPos());
 				PooterAtt->SetPos(GetPos());
 				int a = 0;
 			}
@@ -123,7 +123,7 @@ void Pooter::Update(float _DeltaTime)
 				M_Pooter->ChangeAnimation("M_Pooter_L_Shoot");
 				float4 PlayerPos_P1 = Isaac::MainPlayer->GetPos();
 				BloodTear* PooterAtt1 = GetLevel()->CreateActor<BloodTear>();
-				PooterAtt1->SetBloodMoveDir(PlayerPos_P1);
+				PooterAtt1->SetBloodMoveDir(PlayerPos_P1 - GetPos());
 				PooterAtt1->SetPos(GetPos());
 			}
 		}
